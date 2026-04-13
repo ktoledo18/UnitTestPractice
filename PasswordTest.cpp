f1fff0b3ba0396c079b3670330850c696d3b6025
Kenai Toledo <ktoledo@csuchico.edu>
2026-04-13 11:47:25 -0700

    save for wednesday

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -17,6 +17,61 @@ class PasswordTest : public ::testing::Test
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+
+TEST(PasswordTest, aab_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("aab"));
+}
+
+TEST(PasswordTest, a_password)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("a"));
+}
+
+TEST(PasswordTest, Aaab_password)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters("Aaab"));
+}
+
+TEST(PasswordTest, percent_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("%%ab"));
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	ASSERT_EQ(1, my_password.count_leading_characters(""));
+}
+
+//__________________________________________________________________________________________________________________________
+
+TEST(PasswordTest, one_and_one_password)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.count_leading_characters("Ab"));
+}
+
+TEST(PasswordTest, no_cap__password)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.count_leading_characters("bb"));
+}
+
+TEST(PasswordTest, no_lower_password)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.count_leading_characters("AA"));
+}
+
+TEST(PasswordTest, hard_password)
+{
+	Password my_password;
+	ASSERT_TRUE(my_password.count_leading_characters("%*%#*(/[,.]/;/*"));
 }
